481ed0b78ffa9f19a6c1be1e8a4b2d7964ba673d
Youny <loonyuni@users.noreply.github.com>
2015-07-08 09:14:56 -0700

    Delete export_tokens.bat

--- export_tokens.bat
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-echo "creating environment variables"
-export SLACK_USER_TOKEN_STRING="xoxp-4413683711-4892450549-6924848996-01df1a"
-export SLACK_URL_TOKEN_STRING="lKXG7xwBRx8FZv4A85G9zNmk"
